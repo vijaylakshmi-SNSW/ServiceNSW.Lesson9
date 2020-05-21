802fd7e56fa9eb327c4c5e675a1e86a549d12d5d
Vijaylakshmi <Vijaylakshmi.Govindraj@service.nsw.gov.au>
2020-05-22 07:05:04 +1000

    cont.

---
+++ Person.js
@@ -0,0 +1,33 @@
+const random = require("./Random");
+
+
+class Person {
+    constructor(firstName, lastName, age, id = uuid.v4()) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.id = id;
+    }
+
+    getInfo() {
+        return `${this.firstName} ${this.lastName} is ${this.age} years old`;
+    }
+
+    static generateRandomPeople(numberOfPeople) {
+        let randomPeople = [];
+        for (let i = 0; i < numberOfPeople; i++) {
+            // Generate a random person
+            let randomPerson = new Person(
+                Random.getRandomGivenName(),
+                Random.getRandomFamilyName(),
+                Random.getRandomNumber(42) + 18
+            );
+            randomPeople.push(randomPerson);
+        }
+        return randomPeople;
+    }
+}
+
+
+
+module.exports = Person;
